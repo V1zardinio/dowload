ea8432a085d37d203b4951d042a25a715f97000e
V1zardinio <v1zard6@mail.ru>
2022-03-31 15:51:41 +0300

    Update client.py

--- client.py
+++ client.py
@@ -2,5 +2,5 @@ import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect(('192.168.100.11', 32192))
-    text = input('Сообщение: ')
-    s.sendall(b(text))
+    text = input(b'Сообщение: ')
+    s.sendall(text)
