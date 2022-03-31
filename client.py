c64aa7374d8c6068813ef504c3213b75d2a54ed2
V1zardinio <v1zard6@mail.ru>
2022-03-31 15:22:00 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,5 @@
 import socket
 
-HOST = '192.168.100.11'  # The server's hostname or IP address
-PORT = 32192             # The port used by the server
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+    s.connect(('192.168.100.11', 32192))
     s.sendall(b'say hello')
