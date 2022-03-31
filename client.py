d7f1893176498265a857b964f92cec0bb09b9aeb
V1zardinio <v1zard6@mail.ru>
2022-03-31 17:19:19 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(('26.247.253.157/8', 32192))
+    s.connect(('178.71.101.104', 32192))
     text = input('Сообщение: ')
     s.sendall(text.encode("utf-8"))
 
